d7f148c940e0cfd7f991062cf973b32d28451a8b
Zulfiqor <siapaa249@gmail.com>
2025-10-15 13:21:23 +0700

    🧪 testing

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,13 +1,33 @@
-/**
- * @format
- */
+import { fireEvent, render } from "@testing-library/react-native";
+import { Home } from "../src/screens";
 
-import React from 'react';
-import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+jest.mock("../src/store/api/homeApi.ts", () => ({
+  useGetHomeQuery: jest.fn(),
+  useGetDetailQuery: jest.fn()
+}));
 
-test('renders correctly', async () => {
-  await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
-  });
-});
+describe('Home Screen', () => {
+  it('Home button navigate to detail screen', () => {
+    const mockData = {
+      results: [{ name: 'Pikachu' }]
+    }
+
+    const navigate = jest.fn();
+    const navigation = { navigate }
+
+    const storeApi = require('../src/store/api/homeApi.ts');
+    storeApi.useGetHomeQuery.mockReturnValue({ data: mockData })
+    storeApi.useGetDetailQuery.mockReturnValue({
+      data: { sprites: { front_default: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" } }
+    })
+
+    const { getAllByTestId } = render(
+      <Home navigation={navigation as any} route={navigation as any} />
+    )
+
+    const button = getAllByTestId("button-list")
+    fireEvent.press(button[0]);
+
+    expect(navigate).toHaveBeenCalledWith('Detail', { name: "Pikachu" })
+  })
+})
